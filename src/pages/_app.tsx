52f2232ccef6547a53eb21f9f2c8e49c18ba709b
Guilherme Mariano <cmariano.gui@gmail.com>
2021-06-06 15:44:38 -0300

    feat: creat _app with theme provider

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Next App</title>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default App
