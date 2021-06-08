1a3656e710ce858ec898669e4234149b92e4a1de
Guilherme Mariano <cmariano.gui@gmail.com>
2021-06-08 11:10:13 -0300

    feat: active webpack 5 in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,9 @@ module.exports = {
   },
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials', "@storybook/addon-links"],
+  core: {
+    builder: "webpack5",
+  },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
