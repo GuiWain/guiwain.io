2cce32ab8d90716010c40407940a5e731e6c9ce3
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-22 22:06:15 -0300

    refactor: removing unused items and updating fonts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,12 @@
 export default {
-  border: {
-    smallRadius: '0.4rem',
-    radius: '0.8rem'
-  },
   font: {
-    family: "'Open Sans', 'Roboto', 'Helvetica Neue', sans-serif",
+    family01: 'Montserrat, sans-serif',
+    family02: 'Merriweather, serif',
     light: 300,
-    normal: 400,
-    semiBold: 600,
+    regular: 400,
     bold: 700,
     extraBold: 800,
+    black: 900,
     sizes: {
       xxxsmall: '1.2rem',
       xxsmall: '1.4rem',
@@ -43,20 +40,5 @@ export default {
     successMedium: '#00A03E',
     errorLight: '#FB3C1E',
     errorMedium: '#ED3020'
-  },
-  spacings: {
-    xxxsmall: '0.5rem',
-    xxsmall: '0.8rem',
-    xsmall: '1.6rem',
-    small: '2.4rem',
-    medium: '3.2rem',
-    large: '4.0rem',
-    xlarge: '4.8rem',
-    xxlarge: '5.6rem'
-  },
-  transition: {
-    slow: '0.7s ease-in-out',
-    default: '0.4s ease-in-out',
-    fast: '0.1s ease-in-out'
   }
 } as const
