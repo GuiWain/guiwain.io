9f6a56f17dcf0c97589bc3e35f7c06f2e3939518
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-23 12:40:32 -0300

    refactor: imported theme update

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,9 +1,9 @@
 import { ThemeProvider } from 'styled-components'
 import { render, RenderResult } from '@testing-library/react'
-import theme from 'styles/theme'
+import * as theme from 'styles/theme'
 
 const renderWithTheme = (children: React.ReactNode): RenderResult =>
-  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
+  render(<ThemeProvider theme={theme.light}>{children}</ThemeProvider>)
 
 export * from '@testing-library/react'
 export { renderWithTheme as render }
