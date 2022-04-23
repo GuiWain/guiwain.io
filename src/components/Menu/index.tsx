77dd2be07834e721ab1264398dc5c03e6f0864ff
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-23 20:33:46 -0300

    feat: menu component creation

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,46 @@
 import * as S from './styles'
 
-const Menu = () => (
-  <S.Wrapper>
-    <h1>Menu</h1>
-  </S.Wrapper>
-)
+import { parseCookies, setCookie } from 'nookies'
+
+export type Theme = 'light' | 'dark'
+
+export type MenuProps = {
+  checked: boolean
+  getTheme: (theme: Theme) => void
+}
+
+const Menu = ({ checked, getTheme }: MenuProps) => {
+  const handleTheme = () => {
+    const cookies = parseCookies()
+    const ONE_MONTH = 60 * 60 * 24 * 4
+
+    // * getting the current theme
+    const currentyTheme = cookies.theme ? cookies.theme : 'light'
+
+    // * reversing the themes
+    const updatedTheme = currentyTheme === 'light' ? 'dark' : 'light'
+
+    setCookie(null, 'theme', updatedTheme, {
+      maxAge: ONE_MONTH
+    })
+
+    !!getTheme && getTheme(updatedTheme)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Logo href="/">
+        <S.Anchor>GuiWain</S.Anchor>
+      </S.Logo>
+
+      <S.Switch
+        checkedChildren={<S.DarkIcon />}
+        unCheckedChildren={<S.LightIcon />}
+        onChange={handleTheme}
+        checked={checked}
+      />
+    </S.Wrapper>
+  )
+}
 
 export default Menu
