882e82fed65759fdb77585e4518a3f38f81ecc37
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-23 20:34:57 -0300

    test: first test - menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -2,9 +2,14 @@ import { render } from 'utils/test-utils'
 
 import Menu from '.'
 
+const mockArgs = {
+  checked: true,
+  getTheme: jest.fn()
+}
+
 describe('<Menu />', () => {
   it('should render the Menu', () => {
-    const { container } = render(<Menu />)
+    const { container } = render(<Menu {...mockArgs} />)
 
     expect(container.firstChild).toMatchSnapshot()
   })
