8b206a25a6c87c1ceb6af457ff40d9e0e09b8773
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-23 12:43:56 -0300

    feat: creation of the initial structure - Menu

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import Menu from '.'
+
+export default {
+  title: 'Components/Menu',
+  component: Menu
+} as Meta
+
+export const Default: Story = () => <Menu />
