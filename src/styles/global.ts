620e9401b13141fe488d0e23dc8e468c4f82a2eb
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-22 22:05:34 -0300

    feat: import new fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,64 +1,77 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-      @font-face {
-    font-family: 'Open Sans';
+  @font-face {
+    font-family: 'Merriweather';
+    font-style: normal;
+    font-weight: 300;
+    src: url('/fonts/Merriweather-Light.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Merriweather';
     font-style: normal;
     font-weight: 400;
-    font-display: swap;
-    src: local('Open Sans Regular'), local('Open-Sans-Regular'),
-        url('/fonts/OpenSans-Regular.ttf') format('truetype');
+    src: url('/fonts/Merriweather-Regular.ttf') format('truetype');
   }
+
   @font-face {
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: local('Open Sans SemiBold'), local('Open-Sans-SemiBold'),
-        url('/fonts/OpenSans-SemiBold.ttf') format('truetype');
-  }
-  @font-face {
-    font-family: 'Open Sans';
+    font-family: 'Merriweather';
     font-style: normal;
     font-weight: 700;
-    font-display: swap;
-    src: local('Open Sans Bold'), local('Open-Sans-Bold'),
-        url('/fonts/OpenSans-Bold.ttf') format('truetype');
+    src: url('/fonts/Merriweather-Bold.ttf') format('truetype');
   }
+
   @font-face {
-    font-family: 'Open Sans';
+    font-family: 'Merriweather';
+    font-style: normal;
+    font-weight: 900;
+    src: url('/fonts/Merriweather-Black.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    src: url('/fonts/Montserrat-Bold.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
     font-style: normal;
     font-weight: 800;
-    font-display: swap;
-    src: local('Open Sans ExtraBold'), local('Open-Sans-ExtraBold'),
-        url('/fonts/OpenSans-ExtraBold.ttf') format('truetype');
+    src: url('/fonts/Montserrat-ExtraBold.ttf') format('truetype');
   }
 
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 900;
+    src: url('/fonts/Montserrat-Black.ttf') format('truetype');
+  }
 
-      &::before,
-      &::after {
-        box-sizing: inherit;
-      }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+      scroll-behavior: smooth;
     }
 
-    ${({ theme }) => css`
-      html {
-        font-size: 62.5%;
-        scroll-behavior: smooth;
-      }
-
-      body {
-        font-family: ${theme.font.family};
-        font-size: ${theme.font.sizes.medium};
-      }
-    `}
-
-  `
+    body {
+      font-family: ${theme.font.family02};
+      font-size: ${theme.font.sizes.xxsmall};
+    }
+  `}
+`
 
 export default GlobalStyles
