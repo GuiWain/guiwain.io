2a3b049dbf72776732d165da5087f26bf5730934
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-23 23:35:00 -0300

    feat: footer component creation

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>
+    <S.Nav>
+      <S.LanguageIcon />
+      <S.AboutIcon />
+      <S.FilterIcon />
+      <S.ArrowIcon />
+    </S.Nav>
+  </S.Wrapper>
+)
+
+export default Footer
