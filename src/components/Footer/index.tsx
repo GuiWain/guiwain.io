bb39c116caf3cb11ac76f68712f68eb8b8e82dbd
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-24 15:53:33 -0300

    feat: addition of React.memo to avoid rerendering

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,17 @@
+import React from 'react'
 import * as S from './styles'
 
-const Footer = () => (
-  <S.Wrapper>
-    <S.Nav>
-      <S.LanguageIcon />
-      <S.AboutIcon />
-      <S.FilterIcon />
-      <S.ArrowIcon />
-    </S.Nav>
-  </S.Wrapper>
-)
+const Footer = () => {
+  return (
+    <S.Wrapper>
+      <S.Nav>
+        <S.LanguageIcon />
+        <S.AboutIcon />
+        <S.FilterIcon />
+        <S.ArrowIcon />
+      </S.Nav>
+    </S.Wrapper>
+  )
+}
 
-export default Footer
+export default React.memo(Footer)
