87be7c6b2cbf1662f03094add137b5686ffb2982
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-24 17:10:18 -0300

    feat: mock insertion for component

--- src/components/CardPost/stories.tsx
+++ src/components/CardPost/stories.tsx
@@ -1,9 +1,20 @@
 import { Story, Meta } from '@storybook/react'
-import CardPost from '.'
+import CardPost, { CardPostProps } from '.'
+
+import { Tags } from 'components/CardPost'
 
 export default {
   title: 'Components/CardPost',
   component: CardPost
 } as Meta
 
-export const Default: Story = () => <CardPost />
+export const Default: Story<CardPostProps> = (args) => <CardPost {...args} />
+
+Default.args = {
+  title: 'Nullam in nisl eget tellus bibendum.',
+  description:
+    'Aliquam felis lorem, viverra ac est at, ornare ultrices felis. Donec cursus purus id tempus posuere. Cras nec felis metus.',
+  date: 'Novembro 10, 2022',
+  reading_time: 10,
+  tags: ['dev'] as Tags[]
+}
