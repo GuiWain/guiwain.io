ee16a78c66f89064e23be3a42b5fe8b8db83515f
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-24 15:54:48 -0300

    refactor: removal of fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,55 +1,6 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'Merriweather';
-    font-style: normal;
-    font-weight: 300;
-    src: url('/fonts/Merriweather-Light.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Merriweather';
-    font-style: normal;
-    font-weight: 400;
-    src: url('/fonts/Merriweather-Regular.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Merriweather';
-    font-style: normal;
-    font-weight: 700;
-    src: url('/fonts/Merriweather-Bold.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Merriweather';
-    font-style: normal;
-    font-weight: 900;
-    src: url('/fonts/Merriweather-Black.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 700;
-    src: url('/fonts/Montserrat-Bold.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 800;
-    src: url('/fonts/Montserrat-ExtraBold.ttf') format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 900;
-    src: url('/fonts/Montserrat-Black.ttf') format('truetype');
-  }
-
   * {
     margin: 0;
     padding: 0;
