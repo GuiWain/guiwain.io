5e94a5ab1ed53dee6fee7787758d65b206f50923
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-24 16:59:09 -0300

    feat: creating a mock list of posts

---
+++ src/templates/HomePage/mockPosts.ts
@@ -0,0 +1,29 @@
+import { Tags } from 'components/CardPost'
+
+export const posts = [
+  {
+    title: 'Nullam in nisl eget tellus bibendum.',
+    description:
+      'Aliquam felis lorem, viverra ac est at, ornare ultrices felis. Donec cursus purus id tempus posuere. Cras nec felis metus.',
+    date: 'Novembro 10, 2022',
+    reading_time: 10,
+    tags: ['dev'] as Tags[]
+  },
+  {
+    title: 'Morbi consequat dolor condimen consectetur consectetur. ',
+    description:
+      'Aliquam felis lorem, viverra ac est at, ornare ultrices felis. Donec cursus purus id tempus posuere. Cras nec felis metus.',
+    date: 'Novembro 10, 2022',
+    reading_time: 7,
+    tags: ['travel'] as Tags[]
+  },
+  {
+    title:
+      'Nullam in nisl eget tellus bibendum. Donec cursus purus id tempus posuere. ',
+    description:
+      'Aliquam felis lorem, viverra ac est at, ornare ultrices felis. Donec cursus purus id tempus posuere. Cras nec felis metus.',
+    date: 'Novembro 10, 2022',
+    reading_time: 4,
+    tags: ['life'] as Tags[]
+  }
+]
