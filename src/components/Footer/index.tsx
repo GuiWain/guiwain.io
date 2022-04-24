21e1fd141e518b2b6ead796780f6402891cb56c2
Guilherme Mariano <cmariano.gui@gmail.com>
2022-04-24 19:21:22 -0300

    feat: address creation and scrollToTop function

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,14 +2,28 @@ import React from 'react'
 import * as S from './styles'
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0)
+  }
+
   return (
     <S.Wrapper>
-      <S.Nav>
-        <S.LanguageIcon />
-        <S.AboutIcon />
-        <S.FilterIcon />
-        <S.ArrowIcon />
-      </S.Nav>
+      <S.Address>
+        <S.Author>Guilherme Mariano</S.Author>
+        <S.Year>2022-2022</S.Year>
+        <S.Anchor href="https://github.com/GuiWain/guiwain.io" target="_blank">
+          <S.GithubIcon />
+        </S.Anchor>
+      </S.Address>
+
+      <S.Footer>
+        <S.Nav>
+          <S.LanguageIcon />
+          <S.AboutIcon />
+          <S.FilterIcon />
+          <S.ArrowIcon onClick={scrollToTop} />
+        </S.Nav>
+      </S.Footer>
     </S.Wrapper>
   )
 }
