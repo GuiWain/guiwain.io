0790e7b16beb2cdf1daa66d3161029a2c388cd33
Guilherme Mariano <cmariano.gui@gmail.com>
2022-05-08 19:02:00 -0300

    feat: addition of mock

--- src/templates/Post/stories.tsx
+++ src/templates/Post/stories.tsx
@@ -1,10 +1,20 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 import Post from '.'
+import * as m from './mockPost'
 
 export default {
   title: 'Components/Post',
   component: Post
 } as ComponentMeta<typeof Post>
 
-export const Default: ComponentStory<typeof Post> = () => <Post />
+export const Default: ComponentStory<typeof Post> = (args) => <Post {...args} />
+
+Default.args = {
+  title: m.post2.title,
+  resume: m.post2.resume,
+  content: m.post2.content,
+  date: m.post2.date,
+  reading_time: m.post2.reading_time,
+  updateDate: m.post2.updateDate
+}
