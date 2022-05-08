95f7fc2a2350e9c20045142ccf786937b8664703
Guilherme Mariano <cmariano.gui@gmail.com>
2022-05-08 19:00:15 -0300

    test: test update with mock

--- src/components/CardPost/test.tsx
+++ src/components/CardPost/test.tsx
@@ -6,6 +6,7 @@ import { Tags } from 'components/CardPost'
 
 const mockPost = {
   title: 'Nullam in nisl eget tellus bibendum.',
+  slug: '1',
   description:
     'Aliquam felis lorem, viverra ac est at, ornare ultrices felis. Donec cursus purus id tempus posuere. Cras nec felis metus.',
   date: 'Novembro 10, 2022',
--- src/templates/Post/test.tsx
+++ src/templates/Post/test.tsx
@@ -1,10 +1,20 @@
 import { render } from 'utils/test-utils'
 
 import Post from '.'
+import * as m from './mockPost'
+
+const mockPost = {
+  title: m.post2.title,
+  resume: m.post2.resume,
+  content: m.post2.content,
+  date: m.post2.date,
+  reading_time: m.post2.reading_time,
+  updateDate: m.post2.updateDate
+}
 
 describe('<Post />', () => {
   it('should render the Post', () => {
-    const { container } = render(<Post />)
+    const { container } = render(<Post {...mockPost} />)
 
     expect(container.firstChild).toMatchSnapshot()
   })
